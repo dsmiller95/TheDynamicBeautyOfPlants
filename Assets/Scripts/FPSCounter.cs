5eb8a427835e9e14c52395aec23f5069aea6faac
Dan Miller <dsmiller95@gmail.com>
2022-11-03 23:39:27 -0500

    add fps counter. fixup questions slide to include self promo.

--- Assets/Scripts/FPSCounter.cs
+++ Assets/Scripts/FPSCounter.cs
@@ -1,21 +1,50 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FPSCounter : MonoBehaviour
 {
+    public Queue<float> fpsMeasurements = new Queue<float>();
+    public int frameDepth = 20;
     public TMPro.TMP_Text text;
 
 
-    private DateTime started;
-    private void Start()
-    {
-        started = DateTime.Now;
-    }
+    private int frameCount = 0;
+    private float nextUpdate = 0.0f;
+    public float updateRate = 4;  // 4 updates per sec.
 
     private void Update()
     {
-        var delta = DateTime.Now - started;
-        text.text = $"<mspace=17>{delta.ToString(@"mm")}</mspace>:<mspace=17>{delta.ToString(@"ss")}</mspace>";
+        frameCount++;
+        if (Time.time <= nextUpdate)
+        {
+            return;
+        }
+
+        nextUpdate += 1 / updateRate;
+        fpsMeasurements.Enqueue(frameCount * updateRate);
+        if (fpsMeasurements.Count > frameDepth) fpsMeasurements.Dequeue();
+        frameCount = 0;
+        UpdateFrameText();
+
+    }
+
+    private void UpdateFrameText()
+    {
+        var min = float.MaxValue;
+        var max = float.MinValue;
+
+        foreach (var fps in fpsMeasurements)
+        {
+            min = Mathf.Min(fps, min);
+            max = Mathf.Max(fps, min);
+        }
+
+
+        var fpsMax = Mathf.RoundToInt(max);
+        var fpsMid = Mathf.RoundToInt(fpsMeasurements.Peek());
+        var fpsMin = Mathf.RoundToInt(min);
+
+        text.text = $"<mspace=17>{fpsMin,3}:{fpsMid,3}:{fpsMax,3}</mspace>";
     }
 }
 
